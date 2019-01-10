58253ce7811be5c807f095e618a22a246b05ca5a
Igor Manzhos <igor.manzhos@globallogic.com>
2019-01-10 18:53:50 +0200

    another way to create service

--- src/app/services/new-service.service.ts
+++ src/app/services/new-service.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable, Optional } from '@angular/core';
+import {Inject, Injectable, Optional} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewServiceService {
   private _names = ['name1', 'name2', 'name3'];
+  private _count;
 
   get count(): number {
     return this._count;
@@ -14,10 +15,13 @@ export class NewServiceService {
     return this._names;
   }
 
-  constructor(
-    @Inject('_count') @Optional() public _count?: number,
-  ) {
-    this._count = (_count) ? _count : 10;
+  // constructor(
+  //   @Inject('_count') @Optional() public _count?: number,
+  // ) {
+  //   this._count = (_count) ? _count : 5;
+  // }
+  constructor() {
+    this._count = 10;
   }
 
   add_sq(x: number) {
