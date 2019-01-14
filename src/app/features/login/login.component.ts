fdf73f9cb95e38ad4925bf966bb908ce5a0edbbc
Rosty <Rostyslav.Siryk@externals.hilti.com>
2019-01-14 16:05:47 +0200

    Implement Login form for Login Dialog - add Email field with custom validation

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ErrorStateMatcher, MatDialogRef } from '@angular/material';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class LoginErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +17,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
 
-  ngOnInit() {
+  public matcher = new LoginErrorStateMatcher();
+
+  constructor(
+    private loginDialogRef: MatDialogRef<LoginComponent>,
+    private fb: FormBuilder
+  ) {}
+
+  public ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: [
+        '', [
+          Validators.required,
+          Validators.email,
+        ]
+      ],
+      password: [
+        '',
+        Validators.required
+      ]
+    });
   }
 
 }
